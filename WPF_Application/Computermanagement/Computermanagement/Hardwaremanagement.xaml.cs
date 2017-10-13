5e6f792bc00c633ac5b40467074d460beb9a0ae9
fabiantschofenig <tschofe18@gmail.com>
2017-10-13 12:15:53 +0200

    Hexxor Grid Layout #suchwow

--- WPF_Application/Computermanagement/Computermanagement/Hardwaremanagement.xaml.cs
+++ WPF_Application/Computermanagement/Computermanagement/Hardwaremanagement.xaml.cs
@@ -21,6 +21,7 @@ namespace Computermanagement
     public partial class Hardwaremanagement : Window
     {
         List<Hardware> listOfHardware;
+        Hardware selectedHardware = null;
         public Hardwaremanagement()
         {
             InitializeComponent();
@@ -34,17 +35,49 @@ namespace Computermanagement
             Hardwaremanager.addHardware(new Hardware(2, "Test02", "Test02.jpg", "Test02 pic"));
             Hardwaremanager.addHardware(new Hardware(3, "Test03", "Test03.jpg", "Test03 pic"));
             refreshGUI();
+            setStatusOfInputs(false);
+        }
+
+        private void setStatusOfInputs(bool status)
+        {
+            this.label_description.IsEnabled = status;
+            this.label_name.IsEnabled = status;
+            this.label_logopath.IsEnabled = status;
+
+            this.textbox_description.IsEnabled = status;
+            this.textbox_logopath.IsEnabled = status;
+            this.textbox_name.IsEnabled = status;
         }
 
         private void refreshGUI()
         {
             this.listOfHardware = Hardwaremanager.getallHardware();
             this.lb_ListOfHardware.ItemsSource = this.listOfHardware;
+
+            if(this.selectedHardware != null)
+            {
+                setStatusOfInputs(true);
+                this.textbox_description.Text = this.selectedHardware.description;
+                this.textbox_name.Text = this.selectedHardware.name;
+                this.textbox_logopath.Text = this.selectedHardware.logopath;
+            }
+            else
+            {
+                setStatusOfInputs(false);
+                this.textbox_description.Text = "";
+                this.textbox_name.Text = "";
+                this.textbox_logopath.Text = "";
+            }
         }
 
         private void hardwareSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
+            if (this.lb_ListOfHardware.SelectedIndex != -1)
+                this.selectedHardware = this.listOfHardware[this.lb_ListOfHardware.SelectedIndex];
+            else
+                this.selectedHardware = null;
+
+            refreshGUI();
         }
     }
 }
